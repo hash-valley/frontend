cd80a82caebf34f25b5933b2059566ec7e075804
Nate Mueller <inathan.m@gmail.com>
2022-06-08 10:23:04 +0200

    fix thing

--- src/Utils/multicall.ts
+++ src/Utils/multicall.ts
@@ -10,10 +10,11 @@ const viewContract = new Contract(MulticallAddress, multiABI, viewProvider);
 export const getFarmingStatsMulti = async (tokenIds: (number | string)[]) => {
   const res = await viewContract.getFarmingStats(tokenIds, VineyardAddress);
   return res.map((x: any) => {
-      return {
-        canWater: x === 1,
-        canPlant: x === 2,
-        canHarvest: x === 3,
-      };
+    x = Number(x);
+    return {
+      canWater: x === 1,
+      canPlant: x === 2,
+      canHarvest: x === 3,
+    };
   });
 };
