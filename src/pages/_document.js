45128c9df269253c163283431cbb292aeb5d1599
Nate Mueller <inathan.m@gmail.com>
2022-10-04 16:18:28 -0500

    seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,10 +37,38 @@ class MyDocument extends Document {
           <link rel="preload" href="/fancy-font.ttf" as="font" crossOrigin="" />
           <link rel="preload" href="/NunitoSans-Regular.ttf" as="font" crossOrigin="" />
           <meta name="theme-color" content="#000000" />
+          
+          <title>Hash Valley WInery</title>
+          <meta name="description" content="Cultivate fine wines on Ethereum" />
+
+          <meta property="og:url" content="https://hashvalley.xyz/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Hash Valley Winery" />
           <meta
-            name="description"
-            content="Fine wines on the Ethereum blockchain"
+            property="og:description"
+            content="Cultivate fine wines on Ethereum"
           />
+          <meta
+            property="og:image"
+            content="https://hashvalley.xyz/landscape.png"
+          />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="hashvalley.xyz" />
+          <meta
+            property="twitter:url"
+            content="https://hashvalley.xyz/"
+          />
+          <meta name="twitter:title" content="Hash Valley Winery" />
+          <meta
+            name="twitter:description"
+            content="Cultivate fine wines on Ethereum"
+          />
+          <meta
+            name="twitter:image"
+            content="https://hashvalley.xyz/landscape.png"
+          />
+
           <link rel="apple-touch-icon" href="/logo192.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
