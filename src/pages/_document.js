ccc4b59870450589642c796ca1ca07d974390a8e
Nate Mueller <inathan.m@gmail.com>
2022-12-18 12:16:01 -0800

    seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,28 +37,19 @@ class MyDocument extends Document {
           <link rel="preload" href="/NunitoSans-Regular.ttf" as="font" crossOrigin="" />
           <meta name="theme-color" content="#000000" />
 
-          {/* <meta name="description" content="Cultivate fine wines on Ethereum" /> */}
-          <meta name="description" content="Hash Valley Winery launching soon" />
+          <meta name="description" content="Cultivate fine wines on Ethereum" />
 
           <meta property="og:url" content="https://hashvalley.xyz/" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Hash Valley Winery" />
-          {/* <meta
-            property="og:description"
-            content="Cultivate fine wines on Ethereum"
-          /> */}
-          <meta property="og:description" content="Hash Valley Winery launching soon" />
+          <meta property="og:description" content="Cultivate fine wines on Ethereum" />
           <meta property="og:image" content="https://www.hashvalley.xyz/landscape.png" />
 
           <meta name="twitter:card" content="summary_large_image" />
           <meta property="twitter:domain" content="hashvalley.xyz" />
           <meta property="twitter:url" content="https://hashvalley.xyz/" />
           <meta name="twitter:title" content="Hash Valley Winery" />
-          {/* <meta
-            name="twitter:description"
-            content="Cultivate fine wines on Ethereum"
-          /> */}
-          <meta name="twitter:description" content="Hash Valley Winery launching soon" />
+          <meta name="twitter:description" content="Cultivate fine wines on Ethereum" />
           <meta name="twitter:image" content="https://www.hashvalley.xyz/landscape.png" />
 
           <link rel="apple-touch-icon" href="/logo192.png" />
